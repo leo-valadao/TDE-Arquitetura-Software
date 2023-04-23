facce4f24e4789fe90f49a7457eb1cac2ee5bf27
Leonardo Valadão de Oliveira <leonardovaladaooliveira@hotmail.com>
2023-04-23 20:17:49 -0300

    Atualizar Pastas do Padrão Proxy Com Exemplos do Problema e da Solução

--- Padrao-Proxy/src/solucaoPadraoProxy/Cliente.java
+++ Padrao-Proxy/src/solucaoPadraoProxy/Cliente.java
@@ -3,16 +3,6 @@ package solucaoPadraoProxy;
 public class Cliente {
 	public static void main(String[] args) throws Exception {
 
-		// Método SEM Proxy
-		System.out.println("\n\n\n\n\nExemplo SEM Proxy");
-
-		// Instância o ServidorVideo
-		ServidorVideo vp = new ServidorVideo();
-
-		for (int i = 1; i <= 5; i++) {
-			vp.carregarVideo(1);
-		}
-
 		// Métodos COM Proxy
 		System.out.println("\n\n\n\n\nExemplo COM Proxy");
 
